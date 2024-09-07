f1ea51fce88029b426aa9e3c59165d943b97f448
semihmertdev <semihmert.dev@gmail.com>
2024-09-08 01:53:00 +0300

    Fix bugs and improve

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function GamePage() {
-  const [selectedImage] = useState('https://commonreader.wustl.edu/wp-content/uploads/2023/10/whereswaldo-shutterstock.jpg'); // Tek bir fotoğraf
+  const [selectedImage] = useState('https://commonreader.wustl.edu/wp-content/uploads/2023/10/whereswaldo-shutterstock.jpg');
   const [target, setTarget] = useState(null);
   const [menuVisible, setMenuVisible] = useState(false);
   const [characters] = useState(['Waldo', 'Wizard', 'Wilma']);
@@ -16,6 +16,8 @@ function GamePage() {
   const [playerName, setPlayerName] = useState('');
   const [elapsedTime, setElapsedTime] = useState(0);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [scoreSaved, setScoreSaved] = useState(false);
 
   const navigate = useNavigate();
 
@@ -23,8 +25,8 @@ function GamePage() {
     let timer;
     if (isTimerRunning) {
       timer = setInterval(() => {
-        setElapsedTime(Math.floor((Date.now() - startTime) / 10)); // milisaniyeleri saniyeye çevir
-      }, 10); // milisaniyede bir güncelle
+        setElapsedTime(Math.floor((Date.now() - startTime) / 10));
+      }, 10);
     }
     return () => clearInterval(timer);
   }, [isTimerRunning, startTime]);
@@ -33,11 +35,6 @@ function GamePage() {
     const seconds = Math.floor((time % 6000) / 100);
     const milliseconds = time % 100;
     return `${seconds}s ${milliseconds}ms`;
-  };
-
-  const handleStartGame = () => {
-    setStartTime(Date.now());
-    setIsTimerRunning(true);
   };
 
   const handleImageClick = useCallback((event) => {
@@ -79,93 +76,98 @@ function GamePage() {
   }, [target, characters, markers]);
 
   const handleSaveScore = useCallback(async () => {
+    if (scoreSaved) return;
+
     try {
       await axios.post('https://photo-tagging-backend.onrender.com/api/scores', {
         name: playerName,
-        time: (endTime - startTime), // Süreyi milisaniye cinsinden gönderiyoruz
+        time: (endTime - startTime),
       });
+      setScoreSaved(true);
       setShowHighScoreModal(false);
       navigate('/scoreboard');
     } catch (error) {
       console.error('Error saving score', error);
     }
-  }, [endTime, startTime, playerName, navigate]);
+  }, [endTime, startTime, playerName, navigate, scoreSaved]);
+
+  const handleStartGame = () => {
+    setStartTime(Date.now());
+    setIsTimerRunning(true);
+    setGameStarted(true);
+  };
 
   return (
     <div>
-      {!startTime ? (
-        <div>
-          <h1>Start the Game</h1>
-          <button onClick={handleStartGame}>Start Game</button>
-        </div>
-      ) : (
-        <div>
-          <h1>Find the Character!</h1>
-          <p>Elapsed Time: {formatTime(elapsedTime)}</p>
-          <div style={{ position: 'relative', width: '600px', height: '400px', margin: '0 auto' }}>
-            <img
-              src={selectedImage}
-              alt="game"
-              onClick={handleImageClick}
-              style={{ width: '100%', height: '100%' }}
-            />
+      <h1>Find the Character!</h1>
+      {gameStarted && (
+        <p>Elapsed Time: {formatTime(elapsedTime)}</p>
+      )}
+      {!gameStarted && (
+        <button onClick={handleStartGame}>Start Game</button>
+      )}
+      <div style={{ position: 'relative', width: '600px', height: '400px', margin: '0 auto' }}>
+        <img
+          src={selectedImage}
+          alt="game"
+          onClick={handleImageClick}
+          style={{ width: '100%', height: '100%' }}
+        />
 
-            {target && menuVisible && (
-              <div
-                style={{
-                  position: 'absolute',
-                  top: target.y - 10,
-                  left: target.x - 10,
-                  border: '2px solid red',
-                  width: '20px',
-                  height: '20px',
-                  borderRadius: '50%',
-                }}
-              >
-                <select
-                  onChange={(e) => handleCharacterSelect(e.target.value)}
-                  defaultValue=""
-                  style={{
-                    position: 'absolute',
-                    top: '-30px',
-                    left: '50%',
-                    transform: 'translateX(-50%)',
-                  }}
-                >
-                  <option value="" disabled>Select Character</option>
-                  {characters.map((char, index) => (
-                    <option key={index} value={char}>{char}</option>
-                  ))}
-                </select>
-              </div>
-            )}
-
-            {markers.map((marker, index) => (
-              <div
-                key={index}
-                style={{
-                  position: 'absolute',
-                  top: marker.y - 10,
-                  left: marker.x - 10,
-                  background: 'green',
-                  width: '20px',
-                  height: '20px',
-                  borderRadius: '50%',
-                }}
-              ></div>
-            ))}
-
-            {feedback && <p>{feedback}</p>}
+        {target && menuVisible && (
+          <div
+            style={{
+              position: 'absolute',
+              top: target.y - 10,
+              left: target.x - 10,
+              border: '2px solid red',
+              width: '20px',
+              height: '20px',
+              borderRadius: '50%',
+            }}
+          >
+            <select
+              onChange={(e) => handleCharacterSelect(e.target.value)}
+              defaultValue=""
+              style={{
+                position: 'absolute',
+                top: '-30px',
+                left: '50%',
+                transform: 'translateX(-50%)',
+              }}
+            >
+              <option value="" disabled>Select Character</option>
+              {characters.map((char, index) => (
+                <option key={index} value={char}>{char}</option>
+              ))}
+            </select>
           </div>
+        )}
 
-          {showHighScoreModal && (
-            <div className="modal">
-              <p>Congratulations! You found all characters in {formatTime(elapsedTime)}.</p>
-              <p>Enter your name to save your score:</p>
-              <input value={playerName} onChange={(e) => setPlayerName(e.target.value)} />
-              <button onClick={handleSaveScore}>Save Score</button>
-            </div>
-          )}
+        {markers.map((marker, index) => (
+          <div
+            key={index}
+            style={{
+              position: 'absolute',
+              top: marker.y - 10,
+              left: marker.x - 10,
+              background: 'green',
+              width: '20px',
+              height: '20px',
+              borderRadius: '50%',
+            }}
+          ></div>
+        ))}
+
+        {feedback && <p>{feedback}</p>}
+      </div>
+
+      {showHighScoreModal && (
+        <div className="modal">
+          <p>Congratulations! You found all characters in {formatTime(elapsedTime)}.</p>
+          <p>Enter your name to save your score:</p>
+          <input value={playerName} onChange={(e) => setPlayerName(e.target.value)} />
+          <button onClick={handleSaveScore}>Save Score</button>
         </div>
       )}
     </div>
